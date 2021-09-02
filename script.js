67b0e4b34b870e13310bc7cf9b2af966e802cafd
Cristian Mazzetti Martin <cristian@cloudoo.it>
2021-09-02 12:24:25 +0200

    update file

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function updateRate() {
+
+    var rate = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML = rate + "%";
+
+}
+
+function compute() {
+
+    var amount = parseInt(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
+
+    interest = (amount * ( 1 + (rate/100) * years ) ) - amount;
+
+    var d = new Date();
+    var interest_year = d.getFullYear() + years;
+
+    result = "If you deposit <mark>" + amount + "</mark>, </br>" +
+             "at an interest rate of <mark>" + rate + "%</mark>. </br>" +
+             "You will receive an amount of <mark>" + interest + "</mark>, </br>" +
+             "in the year <mark>" + interest_year + "</mark>";
+
+    document.getElementById("result").innerHTML = result;
     
 }
         
